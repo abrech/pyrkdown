40d9b6adecf472c058112c37553cdac6db80de73
abrech <adrian.brechtken@gmail.com>
2024-12-04 20:05:28 +0100

    plotting works (kinda)!

--- Example.md
+++ Example.md
@@ -3,17 +3,19 @@ Let's write some code!
 
 The following code block will be executed and print statements will be append to this file:
 ```python
-from math import pi
-def fnc():
-    show = ""
-    for i in range(5):
-        show += str(i)
-        if i > 2:
-            print(show)
-    print(pi)
-fnc()
+import matplotlib.pyplot as plt
+for i in range(3):
+  plt.plot([1 + i, 2, 3], [1, 4, 9 - i])
+  plt.show()
+print("hi")
+plt.show()
 ```
+> ### Output  
+> hi  
+> ![a plot](plot.png)
+
 ^ This was generated!  
 Ideen:
 - Zuweisungen suchen und dann im dict checken ob vorhanden, wenn variable einzeln in zeile
   - regex ist dann \n whitespaces varname(a-Z0-9_) whitespaces \n
+- plots aufteilen? plt.clear() oder so?
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
+import uuid
 
 
-def md(file_path: str, lang: str = "python", redo=False):
+def md(file_path: str, lang: str = "python", generate_duplicates=False):
     """
 
     :param file_path:
     :param lang:
-    :param redo: execute even if theres a codeblock output underneath
+    :param generate_duplicates: execute even if theres a codeblock output underneath
     :return:
     """
     content = ""
@@ -21,16 +22,19 @@ def md(file_path: str, lang: str = "python", redo=False):
         print("The given file did not contain any codeblocks.")
         return
 
+    # print statements
     gen_cnt = ""
     for block in blocks:
         gen_cnt += block + "```"
         if block.startswith(lang):
             block = "result_array_with_an_unique_name = []" + block[len(lang):]
             work_around = dict()
-            # smart? way of replacing each occurrence of "print" with a unique variable
             block = block.replace("print", f"result_array_with_an_unique_name.append")
+            block = block.replace("plt.show()",
+                                  f"plt.savefig(f'plot.png');(result_array_with_an_unique_name.remove('![a plot](plot.png)') or result_array_with_an_unique_name.append('![a plot](plot.png)')) if '![a plot](plot.png)' in result_array_with_an_unique_name else result_array_with_an_unique_name.append('![a plot](plot.png)')")
+
             exec(block, work_around)
-            gen_cnt += "\n> " + "  \n".join([str(r) for r in work_around["result_array_with_an_unique_name"]]) + "\n"
+            gen_cnt += "\n> ### Output" + "".join(["  \n> " + str(r) for r in work_around["result_array_with_an_unique_name"]]) + "\n"
     gen_cnt = gen_cnt[:-3]
     with open(file_path, 'w') as f:
         f.write(gen_cnt)
@@ -39,4 +43,11 @@ def md(file_path: str, lang: str = "python", redo=False):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please provide a markdown file location.")
-    md(sys.argv[1])
+    #md(sys.argv[1])
+
+import matplotlib.pyplot as plt
+for i in range(3):
+  plt.plot([1 + i, 2, 3], [1, 4, 9 - i])
+  plt.savefig("test.png")
+print("hi")
+plt.savefig("test2.png")
